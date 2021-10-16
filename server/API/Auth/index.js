b228d81ba05a13671c426f3f338f595f87aa5db9
Celina <celina23042001@gmail,com>
2021-10-16 21:53:24 +0530

    add server

---
+++ server/API/Auth/index.js
@@ -0,0 +1,101 @@
+// Library
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+//import passport from "passport";
+
+// Models
+import { UserModel } from "../../database/user";
+
+// validation
+//import { ValidateSignup, ValidateSignin } from "../../validation/auth";
+
+const Router = express.Router();
+
+/*
+Route     /signup
+Des       Register new user
+Params    none
+Access    Public
+Method    POST  
+*/
+Router.post("/signup", async(req, res) => {
+  try {
+    const { email, password, fullname, phoneNumber } = req.body.credentials; 
+    //await ValidateSignup(req.body.credentials);
+
+    const checkUserByEmail = await UserModel.findOne({ email });
+    const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+
+    if (checkUserByEmail || checkUserByPhone) {
+      return res.json({ error: "User already exists! "});
+    }
+    const bcryptSalt = await bcrypt.genSalt(8);
+
+    const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+    await UserModel.create({...req.body.credentials, password: hashedPassword});
+    //await UserModel.findByEmailAndPhone(req.body.credentials);
+    //const newUser = await UserModel.create(req.body.credentials);
+    const token = jwt.sign({user: {fullname, email}}, "ZomatoAPP");
+    //const token = newUser.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// /*
+// Route     /signin
+// Des       Signin with email and password
+// Params    none
+// Access    Public
+// Method    POST  
+// */
+// Router.post("/signin", async (req, res) => {
+//   try {
+//     //await ValidateSignin(req.body.credentials);
+
+//     const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+
+//     const token = user.generateJwtToken();
+//     return res.status(200).json({ token, status: "success" });
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// });
+
+// /*
+// Route     /google
+// Des       Google Signin
+// Params    none
+// Access    Public
+// Method    GET  
+// */
+// Router.get(
+//   "/google",
+//   passport.authenticate("google", {
+//     scope: [
+//       "https://www.googleapis.com/auth/userinfo.profile",
+//       "https://www.googleapis.com/auth/userinfo.email",
+//     ],
+//   })
+// );
+
+// /*
+// Route     /google/callback
+// Des       Google Signin Callback
+// Params    none
+// Access    Public
+// Method    GET  
+// */
+// Router.get(
+//   "/google/callback",
+//   passport.authenticate("google", { failureRedirect: "/" }),
+//   (req, res) => {
+//     return res.redirect(
+//       `http://localhost:3000/google/${req.session.passport.user.token}`
+//     );
+//   }
+// );
+
+export default Router;
---
+++ server/index.js
@@ -0,0 +1,27 @@
+// Importing env variables
+require("dotenv").config();
+
+// Libraries
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+// Microservices routes
+import Auth from "./API/Auth";
+
+// Database connection
+import ConnectDB from "./database/connection";
+
+const zomato = express();
+
+zomato.use(express.json());
+zomato.use(express.urlencoded({extended: false}));
+zomato.use(helmet());
+zomato.use(cors());
+
+// Application routes
+zomato.use("/auth", Auth);
+
+zomato.get("/", (req, res) => res.json({message: "setup successful"}));
+
+zomato.listen(4000, () => ConnectDB().then(() => console.log("server is running")).catch(() => console.log("server is running, but database connection failed")));
